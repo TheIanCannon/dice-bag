77be87378f79839106b250b524f95a7dae56d285
Ian Cannon <ian.cannon@protonmail.com>
2022-01-31 14:15:26 -0800

    Create and fill out README.md

---
+++ app.js
@@ -0,0 +1,27 @@
+function rollD4(){
+  document.getElementById("d4").innerText= `${(Math.floor(Math.random()*4)+1)}`;
+}
+
+function rollD6(){
+  document.getElementById("d6").innerText= `${(Math.floor(Math.random()*6)+1)}`;
+}
+
+function rollD8(){
+  document.getElementById("d8").innerText= `${(Math.floor(Math.random()*8)+1)}`;
+}
+
+function rollD10(){
+  document.getElementById("d10").innerText= `${(Math.floor(Math.random()*10)+1)}`;
+}
+
+function rollD100(){
+  document.getElementById("d100").innerText= `${(Math.floor(Math.random()*100)+1)}`;
+}
+
+function rollD12(){
+  document.getElementById("d12").innerText= `${(Math.floor(Math.random()*12)+1)}`;
+}
+
+function rollD20(){
+  document.getElementById("d20").innerText= `${(Math.floor(Math.random()*20)+1)}`;
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+*Hello!*
+
+
+_Dice Bag_
+
+This is a simple app to roll virtual electronic dice for tabletop game players.
+
+This app uses the same amounts and types of dice as the basic d4 to d20 set, with the addition of an extra d20 to handle two rolls at once (for advantage and disadvantage based rolls, multiple attacks, and the like).
+
+The set also features a d100 to roll percentile on one die, instead of 2d10 where each is used respectively for a percentile and perdecile.
+
+
+_Getting Started_
+
+To use it, simply start up the index.html file, and click any of the 'dice' on screen. You will be given an instant result! Happy gaming!
